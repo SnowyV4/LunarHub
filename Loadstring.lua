a104350ba068362d81e7ef05c88a5b697024d162
snowy <97677273+SnowyV4@users.noreply.github.com>
2023-12-10 21:38:11 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,8 +1,1 @@
-local p = "Executed LunarHub for"
-if game.PlaceId == 14205206234 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowyV4/LunarHub/main/games/MarlinBay.lua"))()
-    print(p, "Marlin Bay")
-elseif game.PlaceId == 15187265260 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/SnowyV4/LunarHub/main/games/EMWC/loader'))()
-    print(p, "Ez Money Welding Co.")
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SnowyV4/LunarHub/main/CreditsOrSources/loader'))()
