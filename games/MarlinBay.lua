dd2ce5098f9c56ac8846ccd892eb787cc25292ff
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-07-25 01:20:40 -0500

    Update MarlinBay.lua

--- games/MarlinBay.lua
+++ games/MarlinBay.lua
@@ -1,1 +1,26 @@
---soon
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "EASE HUB ", HidePremium = false, IntroText = "Ease Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+ 
+local TradeScamTab = Window:MakeTab({
+    Name = "TRADESCAM",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+ 
+TradeScamTab:AddToggle({
+    Name = "TradeScam",
+    Default = false,
+    Callback = function(Value)
+        print(Value)
+    end
+})
+ 
+TradeScamTab:AddToggle({
+    Name = "FreezeTrade",
+    Default = false,
+    Callback = function(Value)
+        print(Value)
+    end
+})
+
+OrionLib:Init()
